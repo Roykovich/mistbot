8f65475327045fbdf50779ae2305947d4be8f100
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:38:51 -0400

    removed commands, controllers and unnecessary or useless code.

--- events/guildMemberAdd.js
+++
@@ -1,8 +0,0 @@
-const { createCanvas } = require('../core/core');
-module.exports = member => {
-	const channel = member.guild.channels.find(ch => ch.name === 'general');
-	if (!channel) return;
-
-	const attachment = createCanvas(member);
-	channel.send(`Welcome to the server puto, ${member}!`, attachment);
-};
