8075e07245629dca05d62c63622b4be752e5ab2a
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:36:32 -0400

    some tweakings

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,15 +1,20 @@
 const fs = require('fs');
 const guilds = require('../data/guildsData.json');
 const { prefix } = require('../settings.json');
+const guildData = './data/guildsData.json';
 module.exports = guild => {
-	if(!guilds[guild.id]) {
+	if (!guilds[guild.id]) {
 		guilds[guild.id] = {
 			name: guild.name,
 			prefix,
 			channels: [],
 		};
+		console.log(
+			`Guild name: ${guild.name} has been added to the database. 
+			with the ID: ${guild.id}`
+		);
 	}
-	fs.writeFile('./data/guildsData.json', JSON.stringify(guilds, null, 2), (err) => {
+	fs.writeFile(guildData, JSON.stringify(guilds, null, 2), (err) => {
 		if (err) return console.error(err);
 	});
 };
--- events/loadEvents.js
+++ events/loadEvents.js
@@ -3,6 +3,6 @@ module.exports = bot => {
 	bot.on('ready', () => reqEvent('ready')(bot));
 	bot.on('guildCreate', (guild) => reqEvent('guildCreate')(guild));
 	bot.on('guildDelete', (guild) => reqEvent('guildDelete')(guild));
-	bot.on('guildMemberAdd', async (member) => reqEvent('guildMemberAdd')(member));
+	bot.on('guildMemberAdd', async (member) => reqEvent('newMember')(member));
 	bot.on('message', async (message) => reqEvent('message')(message));
 };
