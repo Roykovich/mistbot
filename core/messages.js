b3ac96657cdb696f44a185c169ab6ccd036342f4
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:33:45 -0400

    added new auto-response responses :p

--- core/messages.js
+++ core/messages.js
@@ -1,11 +1,26 @@
+const Chance = require('chance');
+const chance = new Chance();
 const exp = module.exports = {};
+const maldiciones = [
+	'No maldigas maldito.',
+	'¿Por qué tu maldice? No ves que el que maldice se le devuelve la maldición',
+	'El que maldice se le devuelve la maldición',
+];
 
 exp.autoRes = (message, res) => {
-	if (res.match(/(ruben|reuben|guben|runerd|roy),? (arregla|repara|fixea|bichea|acomoda) [ts]u (mierda|vaina|verga|bot|perol)/gi)) {
+	if (res.match(/(rubén|ruben|reuben|guben|runerd|roy),? (arregla|repara|fixea|bichea|acomoda) [ts]u ((maldit[ao])?|(put[ao])?|(verga)?|(estupid[ao])?)\s?(mierda|vaina|verga|bot|perol|robot)/gi)) {
 		message.react('😡').catch((err) => console.error(err));
 		message.channel.send('Está ocupado.');
 	}
-	else if (res.match(/el socialismo funciona?/gi)) {
+	else if (res.match(/¿?el (socialismo|comunismo|chavismo) funciona\??/gi)) {
 		message.channel.send('¿Tú eres marico?');
+	}
+	else if (res.match(/([Mm]+a+[lr]+d+i+t+[ao]+|m+a+t+i+l+d+o+)/gi)) {
+		const maldicion = chance.pickone(maldiciones);
+		message.react('😡').catch((err) => console.error(err));
+		message.channel.send(maldicion);
+	}
+	else if (res.match(/(mam[ae]lo|chupa[tm]el[ao])/gi)) {
+		message.channel.send('Sacatelo ps');
 	}
 };
