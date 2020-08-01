f40cb5d27d9fc399ef2871256329f2b5c0e6840a
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:37:07 -0400

    New help module added

--- events/message.js
+++ events/message.js
@@ -9,9 +9,11 @@ const bot = new Discord.Client();
 const cooldowns = new Discord.Collection();
 bot.commands = new Discord.Collection();
 
-// How to read the commands in the folder excluding the ones that aren't js files
+// How to read the commands in the folder excluding
+// the ones that aren't js files
 const loadCommands = (path) => {
-	const commandFiles = fs.readdirSync(`./commands/${path}/`).filter(file => file.endsWith('.js'));
+	const commandFiles = fs.readdirSync(`./commands/${path}/`)
+		.filter(file => file.endsWith('.js'));
 	for (const file of commandFiles) {
 		const command = rfr(`./commands/${path}/${file}`);
 		bot.commands.set(command.name, command);
@@ -20,26 +22,32 @@ const loadCommands = (path) => {
 
 loadCommands('normal');
 loadCommands('admin');
-loadCommands('warframe');
 loadCommands('own');
+loadCommands('apis');
 
 module.exports = async message => {
+
 	// bots can't trigger commands
-	if(message.author.bot) return;
+	if (message.author.bot) return;
 
 	// autoresponses system
 	if (!message.content.startsWith(guilds[message.guild.id].prefix || prefix)) {
 		autoRes(message, message.content.toLowerCase());
 	}
 	else {
+
 		// arguments and command name
-		const args = message.content.slice(guilds[message.guild.id].prefix.length || prefix.length).split(/ +/);
+		const args = message.content.slice(guilds[message.guild.id].prefix.length
+			|| prefix.length).trim().split(/ +/);
 		const commandName = args.shift().toLowerCase();
 
-		// the name of the command from the collection in commandName and their aliases
+		// the name of the command from the collection in commandName
+		// and their aliases
 		const command = bot.commands.get(commandName)
-    || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-		if(!command) return;
+			|| bot.commands.find(cmd => cmd.aliases
+				&& cmd.aliases.includes(commandName));
+
+		if (!command) return;
 
 		// cooldown system
 		if (!cooldowns.has(command.name)) {
@@ -66,7 +74,7 @@ module.exports = async message => {
 		try {
 			command.execute(message, args);
 		}
-		catch(error) {
+		catch (error) {
 			console.error(error);
 			message.reply('There was an error trying to execute that command!');
 		}
