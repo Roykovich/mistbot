9fc6968ed405a511e47886a9391d9ba5c5667143
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:26:05 -0400

    translator core module added.

---
+++ commands/own/translator.js
@@ -0,0 +1,54 @@
+/* eslint-disable indent */
+const rfr = require('rfr');
+const { translation } = rfr('./core/translator');
+
+/** Todo
+ * 1 - autenticador
+ * 2 - soprote a varios idiomas
+ * 3 - checkear strings
+ * 4 - handlers.
+ */
+
+module.exports = {
+  name: 'translator',
+  description: 'It translates whatever you give to the bot... In theory.',
+  aliases: ['translate', 'traducir', 'traduce'],
+  usage: '[command name] <first language> / <second language> / <text to translate>',
+  cooldown: 15,
+  async execute(message, args) {
+    const arguments = args.join(' ').split(' / ');
+    const firstLan = arguments[0];
+    const secondLan = arguments[1];
+    const result =
+      await translation(arguments[2], firstLan, secondLan);
+
+    if (result == 400) {
+      return message.reply('Insert a correct code!');
+    }
+    const embed = {
+      'author': {
+        'name': 'Mist Bot Translator with Googlesheets',
+      },
+      'color': 12370112,
+      'fields': [
+        {
+          'name': `${result.inputFlag} ${result.inputLang} (${firstLan.toUpperCase()})`,
+          'value': arguments[2],
+          'inline': true,
+        },
+        {
+          'name': `${result.outputFlag} ${result.outputLang} (${secondLan.toUpperCase()})`,
+          'value': result.translation,
+          'inline': true,
+        },
+      ],
+      footer: {
+        'text': 'Esta vaina es una alpha asi que faltan muchas cosas por arreglar',
+      },
+    };
+
+
+    message.channel.send(`<@!${message.author.id}>`, { embed })
+      .catch(err => console.error(err.stack));
+  },
+};
