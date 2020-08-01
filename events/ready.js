510f1be1e15d403cc1b70445a6b63778407aefdf
Rubén Ortiz <runerd.menethil@gmail.com>
2020-08-01 05:37:26 -0400

    Some tweakings

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,24 @@
-const { game } = require('../core/core');
 module.exports = bot => {
-	console.log(`\nBot up and running in ${bot.guilds.size} servers!`);
-	setInterval(() => {
-		bot.user.setActivity(`${game.cetus} | ${game.baro}`, { type: 'WATCHING' });
-	}, 60 * 1000);
+	console.log('\nBot up and running!');
+
+	// const guild = bot.guilds.cache.get('588977362234900490');
+	// const channel = guild.channels.cache.get('688862805339930663');
+	bot.user.setActivity('What\'s my purpose?', { type: 'WATCHING' });
+
+
+	/**
+	 * Repeat message mechanism
+	 */
+
+	// if (guild) {
+	// 	if (channel) {
+	// 		setInterval(() => {
+	// 			channel.send('Esto funciona');
+	// 		}, 10 * 5000);
+	// 	}
+	// }
+	// else {
+	// 	console.log('Theres');
+	// }
+
 };
