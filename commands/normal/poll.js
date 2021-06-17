43d003b9ff535362aed5a3ad55158af0453c8b52
Rubén Ortiz <runerd.menethil@gmail.com>
2021-06-17 17:48:13 -0400

    Add support to / - and | separators and delete the message that the user hast sent for the command

--- commands/normal/poll.js
+++ commands/normal/poll.js
@@ -1,15 +1,17 @@
 /* eslint-disable no-undef */
 module.exports = {
 	name: 'poll',
-	description: 'A simple poll embed. It covers 1 to 10 choices.',
+	description: 'A simple poll embed. It covers 1 to 10 options.',
 	aliases: ['survey', 'votacion'],
-	usage: '<choice 1> / <choice 2> / <choice 3> ... <choice 10>',
+	usage: '<question> / <option 1> / <option 2> / <option 3> ... <option 10>',
 	cooldown: 60,
 	async execute(message, args) {
-		const options = args.join(' ').split(/( ?\/ ?)/);
+		const options = args.join(' ').split(/ *[|/-] */);
 		const letters = ['🇦', '🇧', '🇨', '🇩', '🇪', '🇫', '🇬', '🇭', '🇮', '🇯'];
 		if (options.length <= 2) return message.reply('Add more choices please!');
 		if (options.length > 11) return message.reply('You added to much choices, please use the maximun: **10**');
+
+		message.delete({ timeout: 1000 });
 
 		// We create an object with the current question and an array of options
 		const poll = {
@@ -31,7 +33,7 @@ module.exports = {
 		}
 
 		// The bot sent the message to the channel where we called the poll command
-		const msg = await message.channel.send(`<@!${message.author.id}> has started a poll: **${options[0]}**`, { embed });
+		const msg = await message.channel.send(`@everyone Heeeeey!\n<@!${message.author.id}> has started a poll: **${options[0]}**`, { embed });
 
 		// And after that we react to the message we sent before to make
 		// a system where the users can vote.
